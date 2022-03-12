274f85c89e6e7610fec0f5877d6a83ca90466272
Vebnik(Nik) <vebnik111@gmail.com>
2022-03-12 17:06:33 +0300

    edit save dir

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 const fs = require('fs');
 const path = require(`path`)
 const ytdl = require('ytdl-core')
+const {mkdir} = require("ffmpeg/lib/utils");
 
 
 async function downloadVideo(idVideo, doc) {
 
 	ytdl.getInfo(`${idVideo}`)
 		.then(info => {
-			console.group('YtDownload')
-			console.log(info)
-			console.groupEnd()
-
 			let downloadDetails = {
-				savePath: path.join('media',`${info.videoDetails.title}.mp4`),
+				savePath: path.join(__dirname,'media',`${info.videoDetails.title}.mp4`),
 				counterSize: 0,
 				quality: [],
 				fullDetails: info
 			}
 
-			ytdl(`${idVideo}`, {highWaterMark: 4096,dlChunkSize: 0, quality: 'highest'})
-				.on('start', () => {
-					console.log('Start Download')
+			mkdir(path.join(__dirname, 'media'))
 
-					doc.querySelector('#videoName').textContent = info.videoDetails.title
-				})
+			ytdl(`${idVideo}`, {highWaterMark: 4096,dlChunkSize: 0, quality: 'highest'})
 				.on('progress', (event) => {
 					let size = ((downloadDetails.counterSize += event)/1000/1000).toFixed(2)
 
 					doc.querySelector('#currentSize').textContent = `${size} mb`
+					doc.querySelector('#videoName').textContent = info.videoDetails.title
 				})
 				.on('end', () => {
 					console.log('Download completed')
@@ -37,4 +32,3 @@ async function downloadVideo(idVideo, doc) {
 }
 
 module.exports = {downloadVideo}
-
